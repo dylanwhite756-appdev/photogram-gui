e4d83a90cd7bf97dd6496af03f54665cf3c58e48
Dylanwhite756 <dylanwhite@uchicago.edu>
2022-11-06 22:22:56 +0000

    almost done

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,9 @@ get("/users", { :controller => "users" , :action => "index"})
 get("/users/:path_username", { :controller => "users" , :action => "show"})
 
 get("/photos", { :controller => "photos" , :action => "index"})
+get("/photos/:path_id", { :controller => "photos" , :action => "show"})
 
+get("/delete_photo/:toast_id", { :controller => "photos" , :action => "delete"})
+
+get("/insert_photo", { :controller => "photos" , :action => "create"})
 end
